d5c432d74e65fe36c6053bddf86e71151aff5969
founderkomunitasonline <lubisdesy834@gmail.com>
2026-03-20 19:18:09 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Remove experimental.appDir karena di Next.js 14 sudah default
 }
 
 module.exports = nextConfig
